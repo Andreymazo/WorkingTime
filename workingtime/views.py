96e7839091d884e709980810797a580db3eda905
andrey_mazo <andreymazo@mail.ru>
2023-08-03 21:10:32 +0300

    timesheet/ returns list of request.user timesheets

--- workingtime/views.py
+++ workingtime/views.py
@@ -45,7 +45,10 @@ class Timesheets(SingleTableView):
         queryset = Timesheet.objects.all()
         lst_emloyees_id = [i for i in Employee.objects.all().values_list('id', flat=True)]
         if self.request.user.id in lst_emloyees_id:
-            return Timesheet.objects.get(employee_id=self.request.user.id)
+            print('+++++++++++++++++++++', self.request.user.id)
+            queryset = Timesheet.objects.filter(employee_id=self.request.user.id)
+            return queryset
+
         return queryset
 
     # def get(self, request, *args, **kwargs):
@@ -141,32 +144,32 @@ class EmployeeDelete(DeleteView):
 #     ordering = ('distance',)  # quantity, name
 #     table_pagination = {"per_page": 5}
 #
-#     def get_queryset(self, **kwargs):
-#         """
-#         Return the list of items for this view.
-#
-#         The return value must be an iterable and may be an instance of
-#         `QuerySet` in which case `QuerySet` specific behavior will be enabled.
-#         """
-#         if self.request.method == "GET":
-#
-#             if self.queryset is not None:
-#                 queryset = self.queryset
-#                 if isinstance(queryset, QuerySet):
-#                     queryset = queryset.all()
-#             if self.model is not None:
-#                 queryset = self.model._default_manager.all()
-#             else:
-#                 raise ImproperlyConfigured(
-#                     "%(cls)s is missing a QuerySet. Define "
-#                     "%(cls)s.model, %(cls)s.queryset, or override "
-#                     "%(cls)s.get_queryset()." % {"cls": self.__class__.__name__}
-#                 )
-#             ordering = self.get_ordering()
-#             if ordering:
-#                 if isinstance(ordering, str):
-#                     ordering = (ordering,)
-#                 queryset = queryset.order_by(*ordering)
+    # def get_queryset(self, **kwargs):
+    #     """
+    #     Return the list of items for this view.
+    #
+    #     The return value must be an iterable and may be an instance of
+    #     `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+    #     """
+    #     if self.request.method == "GET":
+    #
+    #         if self.queryset is not None:
+    #             queryset = self.queryset
+    #             if isinstance(queryset, QuerySet):
+    #                 queryset = queryset.all()
+    #         if self.model is not None:
+    #             queryset = self.model._default_manager.all()
+    #         else:
+    #             raise ImproperlyConfigured(
+    #                 "%(cls)s is missing a QuerySet. Define "
+    #                 "%(cls)s.model, %(cls)s.queryset, or override "
+    #                 "%(cls)s.get_queryset()." % {"cls": self.__class__.__name__}
+    #             )
+    #         ordering = self.get_ordering()
+    #         if ordering:
+    #             if isinstance(ordering, str):
+    #                 ordering = (ordering,)
+    #             queryset = queryset.order_by(*ordering)
 #         Values_table.objects.all().delete()
 #
 #         for i in range(1, 10):
